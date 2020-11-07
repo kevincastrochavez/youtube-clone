c8631621040c0263d31e325339eca1532e454da1
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2020-11-07 12:55:12 -0600

    finished the videocard component and the recommendedvideo section

--- src/components/RecommendedVideos.js
+++ src/components/RecommendedVideos.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import VideoCard from './VideoCard'
 
 function RecommendedVideos() {
     return (
         <div className='recommendedVideos'>
-            Videos here
+            <h2>Recommended</h2>
+            <div className="recommendedVideos__videos">
+                <VideoCard />
+            </div>
         </div>
     )
 }
